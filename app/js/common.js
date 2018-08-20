c6c1de979b0d423eeebc1b6ba80c6b7e8154adba
Skeev_ <derkach94@gmail.com>
2018-08-20 19:52:19 +0300

    finished main page; to do: adaptive menu

--- app/js/common.js
+++ app/js/common.js
@@ -1,8 +1,18 @@
-$(function() {
+$(function () {
 
-	// Custom JS
+    // Custom JS
     var $hamburger = $(".hamburger");
-    $hamburger.on("click", function(e) {
+    $hamburger.on("click", function (e) {
         $hamburger.toggleClass("is-active");
+    });
+    $('.flexslider').flexslider({
+        animation: 'fade',
+        animationLoop: true,
+        controlsContainer: $(".reviews-controls"),
+        customDirectionNav: $(".reviews-controls i"),
+        controlNav: false,
+        start: function (slider) {
+            $('body').removeClass('loading');
+        }
     });
 });
